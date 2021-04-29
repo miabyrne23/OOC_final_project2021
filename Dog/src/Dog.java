16eb7e888e30df79dc22d7a66dc75bbf2a4597bd
miaby <miaby@192.168.0.108>
2021-04-29 13:32:17 +0100

    OOC FINAL PROJECT

---
+++ Dog/src/Dog.java
@@ -0,0 +1,24 @@
+
+public class Dog {
+
+	public static void breathes();{
+		System.out.println("Breathes");
+	}
+	
+	public static void eats();{
+		System.out.println("Eats");
+	}
+	
+	public static void legs();{
+		System.out.println("Four Legs");
+	}
+
+public void bark() {
+	System.out.println("Barks!");
+}
+
+
+
+
+
+}
---
+++ Dog/src/Doginherit.java
@@ -0,0 +1,13 @@
+
+public class Doginherit {
+	
+	public static void breathes();
+	System.out.println("Breathes");
+	
+	public static void eats();
+	System.out.println("Eats");
+	
+	public static void legs();
+	System.out.println("Four Legs");
+
+}
